19399c4bfe63bc57d47aa1853579b5813853466b
soniafrin <soniafrin50@gmail.com>
2026-04-17 03:42:00 +0100

    loading

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import FriendsList from "./FriendsList";
+import { FadeLoader } from "react-spinners";
 
 const Friends = () => {
     const [frnds, setFrnds] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const fetchFriends = async () => {
             const res = await fetch('/friends.json');
             const data = await res.json();
             // console.log(data);
             setFrnds(data)
+            setLoading(false)
         }
         fetchFriends()
     }, [])
-    // console.log(frnds);
+    console.log(frnds);
     return (
         <div className="container mx-auto">
             <h2>Your Friends</h2>
-            <div className="grid grid-cols-4 gap-2 justify-center ">
+            {loading ? <div className="flex justify-center items-center"><FadeLoader color="#244D3F" /></div>  : <div className="grid grid-cols-4 gap-2 justify-center ">
                 {
                     frnds.map((friend, index) => (<FriendsList friend={friend} key={index}/>
                         ))
                 }
-            </div>
+            </div>}
         </div>
     )
 }
